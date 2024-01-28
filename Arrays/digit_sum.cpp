dcbda4ef00d73583046d4eb38053cbb4e9f248c2
RounakSharma19 <rounaksharma808@gmail.com>
2024-01-28 19:37:00 +0530

    feat: array proble,s were added

---
+++ Arrays/digit_sum.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int count = 0;
+    int number = 1;
+
+    while (count < n)
+    {
+        int digit_sum = 0;
+        for (int rem = number; rem > 0; rem /= 10)
+        {
+            digit_sum += rem % 10;
+        }
+        if (digit_sum == k)
+        {
+            count++;
+            cout << number << endl;
+        }
+        number++;
+    }
+
+    return 0;
+}
